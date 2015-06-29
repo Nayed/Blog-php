b614bad958400c9837f659213137fee75e17f9e7
Nayed <nayed.saidali@gmail.com>
2015-06-29 23:28:17 +0200

    Autoloading gestion

---
+++ app/Autoloader.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+class Autoloader{
+
+	static function register(){
+		spl_autoload_register(array(__CLASS__, 'autoload'));
+	}
+
+	static function autoload($classname){
+		if(strpos($classname, __NAMESPACE__ . '\\') === 0){
+			$classname = str_replace(__NAMESPACE__ . '\\', '', $classname);
+			$classname = str_replace('\\', '/', $classname);
+			require __DIR__ . '/' . $classname . '.php';
+		}
+	}
+
+}
