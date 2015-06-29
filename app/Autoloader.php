85ca90464fea66992f8f40c8dcadc695237426e1
Nayed <nayed.saidali@gmail.com>
2015-06-29 23:48:26 +0200

    fix ST weird indent

--- app/Autoloader.php
+++ app/Autoloader.php
@@ -4,16 +4,16 @@ namespace App;
 
 class Autoloader{
 
-	static function register(){
-		spl_autoload_register(array(__CLASS__, 'autoload'));
-	}
+    static function register(){
+        spl_autoload_register(array(__CLASS__, 'autoload'));
+    }
 
-	static function autoload($classname){
-		if(strpos($classname, __NAMESPACE__ . '\\') === 0){
-			$classname = str_replace(__NAMESPACE__ . '\\', '', $classname);
-			$classname = str_replace('\\', '/', $classname);
-			require __DIR__ . '/' . $classname . '.php';
-		}
-	}
+    static function autoload($classname){
+        if(strpos($classname, __NAMESPACE__ . '\\') === 0){
+            $classname = str_replace(__NAMESPACE__ . '\\', '', $classname);
+            $classname = str_replace('\\', '/', $classname);
+            require __DIR__ . '/' . $classname . '.php';
+        }
+    }
 
 }
