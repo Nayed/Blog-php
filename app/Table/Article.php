06a3d0db944abdc301658fc95b7904835867225d
Nayed <nayed.saidali@gmail.com>
2015-07-03 00:55:33 +0200

    fix ST weird extra indent

--- app/Table/Article.php
+++ app/Table/Article.php
@@ -4,19 +4,19 @@ namespace App\Table;
 
 class Article{
 
-	public function __get($key){
-		$method = 'get' . ucfirst($key);
-		$this->$key = $this->$method();
-		return $this->$key;
-	}
+    public function __get($key){
+        $method = 'get' . ucfirst($key);
+        $this->$key = $this->$method();
+        return $this->$key;
+    }
 
-	public function getUrl(){
-		return 'index.php?p=article&id=' . $this->id;
-	}
+    public function getUrl(){
+        return 'index.php?p=article&id=' . $this->id;
+    }
     
     public function getPreview(){
-    	$html = '<p>' . substr($this->content, 0, 150) . '...</p>';
-    	$html .= '<p><a href="' . $this->getURL() . ' ">See the rest...</a></p>';
-    	return $html;
+        $html = '<p>' . substr($this->content, 0, 150) . '...</p>';
+        $html .= '<p><a href="' . $this->getURL() . ' ">See the rest...</a></p>';
+        return $html;
     }
 }
