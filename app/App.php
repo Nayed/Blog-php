b9ee1c9d0dbf9894f815c95b17726754f108ab37
Nayed <nayed.saidali@gmail.com>
2015-07-09 00:10:41 +0200

    set better title

--- app/App.php
+++ app/App.php
@@ -9,6 +9,7 @@ class App{
     const DB_PASS = '';
     const DB_HOST = 'localhost';
     private static $database;
+    private static $title = "Awesome blog!";
 
     public static function getDB(){
         if(is_null(self::$database)){
@@ -20,5 +21,13 @@ class App{
     public static function notFound(){
         header("HTTP/1.0 404 Not Found");
         header("Location:index.php?p=404");
+    }
+
+    public static function getTitle(){
+        return self::$title;
+    }
+
+    public static function setTitle($title){
+        self::$title = $title;
     }
 }
