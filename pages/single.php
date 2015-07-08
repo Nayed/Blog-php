7c669197a2c1bc96997b55a1743b1ffe5d35b77a
Nayed <nayed.saidali@gmail.com>
2015-07-08 23:50:36 +0200

    better single article

--- pages/single.php
+++ pages/single.php
@@ -1,9 +1,21 @@
 <?php
 
-$post = $db->prepare('SELECT * FROM articles WHERE id = ?', [$_GET['id']], 'App\Table\Article', true);
+use App\App;
+use App\Table\Article;
+use App\Table\Categorie;
+
+$post = Article::find($_GET['id']);
+if($post === false){
+    App::notFound();
+}
+$categorie = Categorie::find($post->categorie_id);
 
 ?>
 
 <h1><?= $post->title; ?></h1>
+
+<p>
+    <em><?= $categorie->title; ?></em>
+</p>
 
 <h2><?= $post->content; ?></h2>
