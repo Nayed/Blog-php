27bb0cc8584b228740e9d56cf93f26b1c31ca3a5
Nayed <nayed.saidali@gmail.com>
2015-07-11 00:04:51 +0200

    test singleton
    as you can see, it var dump the same key for all the instance of $config

--- app/Config.php
+++ app/Config.php
@@ -5,8 +5,18 @@ namespace App;
 class Config{
 
     private $settings = [];
+    private static $_instance;
+
+    public static function getInstance(){
+        if(is_null(self::$_instance)){
+            self::$_instance = new Config();
+        }
+        return self::$_instance;
+    }
 
     public function __construct(){
+        $this->id = uniqid();
         $this->settings = require dirname(__DIR__) . '/config/config.php';
     }
+    
 }
