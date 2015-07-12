f3bc7dbb8f618af72a1ad689241cf024c5b5aa0c
Nayed <nayed.saidali@gmail.com>
2015-07-13 00:14:46 +0200

    :fire: everywhere

--- app/App.php
+++ app/App.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace App;
+namespace App\Database;
 
 class App{
 
-    private static $_instance;
     public $title = "Awesome blog!";
+    private $db_instance;
+    private static $_instance;
 
     public static function getInstance(){
         if(is_null(self::$_instance)){
@@ -17,5 +18,18 @@ class App{
     public function getTable($name){
         $className = '\\App\\Table' . ucfirst($name) . 'Table';
         return new $className($this->getDb());
+    }
+
+    public function getDb(){
+        $config = Config::getInstance();
+        if(is_null($this->db_instance)){
+            $this->db_instance = new Database\MysqlDatabase(
+                $config->get('db_name'),
+                $config->get('db_user'),
+                $config->get('db_pass'),
+                $config->get('db_host')
+            );
+        }
+        return $this->db_instance;
     }
 }
