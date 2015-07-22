21b2cb48da4d8a912af0e5210e57102e1db64ee0
Nayed <nayed.saidali@gmail.com>
2015-07-22 23:58:58 +0200

    begin view admin

--- pages/admin/posts/index.php
+++ pages/admin/posts/index.php
@@ -1,1 +1,26 @@
-<h1>Here my admin</h1>
+<?php 
+$posts = App::getInstance()->getTable('Post')->all();
+?>
+
+<h1>Manage articles</h1>
+
+<table class="table">
+    <thead>
+        <tr>
+            <td>ID</td>
+            <td>Title</td>
+            <td>Actions</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($posts as $post): ?>
+            <tr>
+                <td><?= $post->id; ?></td>
+                <td><?= $post->title; ?></td>
+                <td>
+                    <a class="btn btn-primary" href="?posts.edit&id=<?= $post->id; ?>">Edit</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
