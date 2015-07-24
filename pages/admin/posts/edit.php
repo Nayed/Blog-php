e1ad85aea2f96dee201a07ad5d96f5a7d2c36d76
Nayed <nayed.saidali@gmail.com>
2015-07-25 01:16:45 +0200

    ugly implement display success

--- pages/admin/posts/edit.php
+++ pages/admin/posts/edit.php
@@ -1,17 +1,24 @@
 <?php
 $postTable =  App::getInstance()->getTable('Post');
 if(!empty($_POST)){
-    $postTable->update($_GET['id'], [
+    $result = $postTable->update($_GET['id'], [
         'title' => $_POST['title'],
         'content' => $_POST['content']
     ]);
+    if($result){
+        ?>
+            <div class="alert alert-success">Post was updated</div>
+        <?php
+    }
 }
 
 $post = $postTable->find($_GET['id']);
+$categories = App::getInstance()->getTable('Categorie')->extract('id', 'title');
 $form = new \Core\HTML\BootstrapForm($post);
 ?>
 <form method="post">
     <?= $form->input('title', 'Article\'s title'); ?>
     <?= $form->input('content', 'Content', ['type' => 'textarea']); ?>
+    <?= $form->select('categorie_id', 'Categorie', $categories); ?>
     <button class="btn btn-primary">Save</button>
 </form>
