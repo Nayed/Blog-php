c7dc2178e86a5773fb7df8c44722a240190ab58d
Nayed <nayed.saidali@gmail.com>
2015-07-29 00:21:31 +0200

    add categ

--- pages/admin/categories/add.php
+++ pages/admin/categories/add.php
@@ -1,24 +1,19 @@
 <?php
-$postTable =  App::getInstance()->getTable('Post');
+$table =  App::getInstance()->getTable('Categorie');
 if(!empty($_POST)){
-    $result = $postTable->create([
-        'title' => $_POST['title'],
-        'content' => $_POST['content'],
-        'categorie_id' => $_POST['categorie_id']
+    $result = $table->create([
+        'title' => $_POST['title']
     ]);
 
     if($result){
-        header('Location: admin.php?p=posts.edit&id=' . App::getInstance()->getDb()->lastInsertId());
+        header('Location: admin.php?p=categories.index');
     }
 }
 
-$categories = App::getInstance()->getTable('Categorie')->extract('id', 'title');
 $form = new \Core\HTML\BootstrapForm($_POST);
 ?>
 
 <form method="post">
-    <?= $form->input('title', 'Article\'s title'); ?>
-    <?= $form->input('content', 'Content', ['type' => 'textarea']); ?>
-    <?= $form->select('categorie_id', 'Categorie', $categories); ?>
+    <?= $form->input('title', 'Category\'s title'); ?>
     <button class="btn btn-primary">Save</button>
 </form>
