0f5797d8fae981308020fb4ad5929b24a5bb8d24
Nayed <nayed.saidali@gmail.com>
2015-08-02 15:37:28 +0200

    add post

--- app/Controller/Admin/PostsController.php
+++ app/Controller/Admin/PostsController.php
@@ -1,6 +1,7 @@
 <?php 
 
 namespace App\Controller\Admin;
+use Core\HTML\BootstrapForm;
 
 class PostsController extends AppController{
 
@@ -12,5 +13,30 @@ class PostsController extends AppController{
     public function index(){
         $posts = $this->Post->all();
         $this->render('admin.posts.index', compact('posts'));
+    }
+
+    public function add(){
+        if(!empty($_POST)){
+            $result = $this->Post->create([
+                'title' => $_POST['title'],
+                'content' => $_POST['content'],
+                'categorie_id' => $_POST['categorie_id']
+            ]);
+
+            if($result){
+                header('Location: admin.php?p=admin.posts.edit&id=' . App::getInstance()->getDb()->lastInsertId());
+            }
+        }
+        $this->loadModel('Categorie');
+        $categories = $this->Categorie->extract('id', 'title');
+        $form = new BootstrapForm($_POST);
+    }
+
+    public function edit(){
+
+    }
+
+    public function delete(){
+
     }
 }
--- app/Views/admin/posts/add.php
+++ app/Views/admin/posts/add.php
@@ -1,21 +1,3 @@
-<?php
-$postTable =  App::getInstance()->getTable('Post');
-if(!empty($_POST)){
-    $result = $postTable->create([
-        'title' => $_POST['title'],
-        'content' => $_POST['content'],
-        'categorie_id' => $_POST['categorie_id']
-    ]);
-
-    if($result){
-        header('Location: admin.php?p=posts.edit&id=' . App::getInstance()->getDb()->lastInsertId());
-    }
-}
-
-$categories = App::getInstance()->getTable('Categorie')->extract('id', 'title');
-$form = new \Core\HTML\BootstrapForm($_POST);
-?>
-
 <form method="post">
     <?= $form->input('title', 'Article\'s title'); ?>
     <?= $form->input('content', 'Content', ['type' => 'textarea']); ?>
