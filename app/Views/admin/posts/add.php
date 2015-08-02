96c303b33e96337a4d7a65b99da07556318c9922
Nayed <nayed.saidali@gmail.com>
2015-08-02 15:44:00 +0200

    no need anymore
    add.php and edit.php have the same code

--- app/Views/admin/posts/add.php
+++
@@ -1,6 +0,0 @@
-<form method="post">
-    <?= $form->input('title', 'Article\'s title'); ?>
-    <?= $form->input('content', 'Content', ['type' => 'textarea']); ?>
-    <?= $form->select('categorie_id', 'Categorie', $categories); ?>
-    <button class="btn btn-primary">Save</button>
-</form>
